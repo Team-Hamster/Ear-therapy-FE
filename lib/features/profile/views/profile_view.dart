f83d28518003b8782bf865d51ad7961586da1471
Si Yeong Park <psy915@swu.ac.kr>
2024-11-18 00:42:48 +0900

    Feat: Profile 화면 ui 구현

--- lib/features/profile/views/profile_view.dart
+++ lib/features/profile/views/profile_view.dart
@@ -24,6 +24,99 @@ class ProfileView extends StatelessWidget {
                 ),
               ),
             ),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Center(
+                      child: CircleAvatar(
+                        radius: 40,
+                        backgroundImage: AssetImage('assets/icon_images/profile.png'),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      '김이혈',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Female',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      '23',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Divider(),
+                    const SizedBox(height: 30),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.edit,
+                          color: AppColors.accentColor
+                        ),
+                        title: Text('프로필 편집'),
+                        onTap: () {
+                          // Navigate to profile edit screen
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.announcement,
+                          color: AppColors.accentColor
+                          ),
+                        title: Text('공지사항'),
+                        onTap: () {
+                          // Navigate to notices screen
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.settings,
+                          color: AppColors.accentColor
+                          ),
+                        title: Text('Settings'),
+                        onTap: () {
+                          // Navigate to settings screen
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
